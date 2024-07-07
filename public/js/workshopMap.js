cebd0b24609f098d787c45e176054951bbf159e7
Joshua210936 <93179779+Joshua210936@users.noreply.github.com>
2024-07-07 20:21:15 +0800

    added basic map

---
+++ public/js/workshopMap.js
@@ -0,0 +1,9 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'),{
+        center: {lat: 1.3801001587360433, lng: 103.84918473018321},
+        zoom: 12,
+        mapId: '7eb9cb155077df0'
+    });
+}
+
+//nyp 1.3801001587360433, 103.84918473018321
